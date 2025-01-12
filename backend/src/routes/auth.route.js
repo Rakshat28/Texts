4c83c0cf43d452c27316de4bf5537d724eb8a40e
Rakshat28 <rakshat07ps@gmail.com>
2025-01-13 01:32:01 +0530

    add check router to check user

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { signup,login,logout,updateProfile } from '../controllers/auth.controller.js';
-import { protectRoute } from '../middleware/auth.middleware.js';
+import { protectRoute} from '../middleware/auth.middleware.js';
+import { checkAuth } from '../controllers/auth.controller.js';
 const router = express.Router();
 
 router.post("/signup",signup);
@@ -10,5 +11,7 @@ router.post("/login",login);
 router.post("/logout",logout);
 
 router.put("/update-profile",protectRoute,updateProfile);
+
+router.get("/check",protectRoute,checkAuth);
 
 export default router;
